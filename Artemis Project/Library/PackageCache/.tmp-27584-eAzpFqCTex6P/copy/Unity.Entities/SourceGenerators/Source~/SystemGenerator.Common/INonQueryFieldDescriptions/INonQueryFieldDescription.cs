b4fc76f3780c76168d9278c539b3ac16dc145249
Colby Bailey <83721925+bagoferasers@users.noreply.github.com>
2024-01-10 09:47:54 -0700

    Launch Tower
    Added sprites for launch tower. Also positioned player (orion) on tower. Init scene.

--- Artemis Project/Library/PackageCache/.tmp-27584-eAzpFqCTex6P/copy/Unity.Entities/SourceGenerators/Source~/SystemGenerator.Common/INonQueryFieldDescriptions/INonQueryFieldDescription.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Unity.Entities.SourceGen.SystemGenerator.Common
-{
-    public interface INonQueryFieldDescription
-    {
-        string GetFieldDeclaration(bool forcePublic = false);
-        string GetFieldAssignment();
-        string GeneratedFieldName { get; }
-    }
-}
