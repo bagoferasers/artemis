6ff2f21dbd106a3e7c52ecf8d4a273142006fb8d
Colby Bailey <83721925+bagoferasers@users.noreply.github.com>
2024-02-09 09:18:21 -0700

    Goodbye Redundancy at the End of Game
    GENERAL
    
    *Created EndGame.unity Scene and Script.
    *Deleted WonGame.unity and LostGame.unity Scenes and Scripts.
    
    SCENES
    
    EndGame.unity
    *Carries same structure and function as LostGame.unity and WonGame.unity Scenes.
    *Now initializes UI text based on boolean won being true or false.
    
    SCRIPTS
    
    EndGame.cs
    *Shows Final Score to UI.
    *Shows Won or Lost text to UI.
    
    GameManager.cs
    *Now calls upon EndGame.cs method EndGameScene( bool won ).
    
    SceneTransitions.cs
    *Deleted LostGameScene() and WonGameScene() methods.
    *Added EndGameScene( bool won ) method to transition to EndGame.unity Scene and initialize texts on that Scene based on the boolean won being true or false.

---
+++ Artemis Project/Assets/Scripts/EndGame.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+/*
+   File: LostGame.cs
+   Description: Script to handle the EndGame Scene.
+   Last Modified: February 9, 2024
+   Last Modified By: Colby Bailey
+   Authors: Colby Bailey
+*/
+
+/// <summary>
+/// Handles the EndGame Scene.
+/// </summary>
+public class EndGame : MonoBehaviour
+{
+    /// <summary>
+    /// The time to wait till moving to the Credits Scene.
+    /// </summary>
+    [ SerializeField ] private float waitTime = 10;
+
+    /// <summary>
+    /// The TextMeshProUGUI component that will hold the player's final score.
+    /// </summary>
+    private TextMeshProUGUI finalScoreText;
+
+    /// <summary>
+    /// The TextMeshProUGUI component that will hold the player's final score.
+    /// </summary>
+    private TextMeshProUGUI WonLostText;
+
+    /// <summary>
+    /// Sets the final player score to UI and waits a certain amount of time before switching to the Credits Scene.
+    /// </summary>
+    void Start( )
+    {
+        finalScoreText = FindAndInit.InitializeTextMeshProUGUI( gameObjectName: "FinalScore", sceneName: "EndGame.cs" );
+        finalScoreText.text = SaveSystem.GetInt( name: "LastPlayerScore" ).ToString( );
+        WonLostText = FindAndInit.InitializeTextMeshProUGUI( gameObjectName: "WonLostText", sceneName: "EndGame.cs" );
+        if( SaveSystem.GetBool( name: "Won" ) == true )
+        {
+            WonLostText.text = "Mission Success!";
+        }
+        else
+        {
+            WonLostText.text = "Mission Failure.";
+        }
+        WaitForCredits( );
+    }
+
+    /// <summary>
+    /// Method that starts a coroutine for the IENumerator to wait a set time before moving to the Credits Scene.
+    /// </summary>
+    private void WaitForCredits( )
+    {
+        StartCoroutine( routine: WaitForCreditsNumerator( ) );
+    }
+
+    /// <summary>
+    /// Waits for a set time before moving to the Credits Scene.
+    /// </summary>
+    private IEnumerator WaitForCreditsNumerator( )
+    {
+        yield return new WaitForSeconds( seconds: waitTime );
+        SceneTransitions.CreditsScene( );
+    }
+}
